ccd059ce6684132c756751c99c5ea9f08ab46904
Shahmeer-Hamza <hamza@97solutions.com>
2024-01-24 19:49:17 +0500

    app doman name changed

--- android/app/src/main/java/com/ninetysevensolutions/groceryhub/MainActivity.java
+++ android/app/src/main/java/com/ninetysevensolutions/groceryhub/MainActivity.java
@@ -1,4 +1,4 @@
-package com.groceryhub;
+package com.ninetysevensolutions.groceryhub;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/ninetysevensolutions/groceryhub/MainApplication.java
+++ android/app/src/main/java/com/ninetysevensolutions/groceryhub/MainApplication.java
@@ -1,4 +1,4 @@
-package com.groceryhub;
+package com.ninetysevensolutions.groceryhub;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
